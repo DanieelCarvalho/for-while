944d95209bb124f84cceca94697e992f918d5531
DanieelCarvalho <Danielscg2012@gmail.com>
2022-06-14 15:25:53 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+ for(var i =1; i <= 10; i++ ) { 
+    console.log(i)
+}
+let numero = 11;
+while (numero > 1) {
+    numero = numero - 1 
+    console.log(numero)
+}
+
+for(var i = 1; i <= 100; i= i +2){
+    console.log(i)
+}
+
+for(var i = 0; i <= 100; i= i + 2){
+    console.log(i)
+}
